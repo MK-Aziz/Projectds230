a23db01b31ee44b955df14127ce138bec67a3920
Abdulaziz Nadreen <Abdulaziz.nadreen1@gmail.com>
2025-11-19 15:05:02 +0300

    added unversioned files

---
+++ src/Employee.java
@@ -0,0 +1,11 @@
+public abstract class Employee extends Person {
+
+    public Employee(String name, int id) {
+        super(name, id);
+    }
+
+    public abstract double calculateSalary();
+
+    @Override
+    public abstract void displayDetails();
+}
---
+++ src/PartTimeEmployee.java
@@ -0,0 +1,42 @@
+public class PartTimeEmployee extends Employee {
+    private double hourlyRate;
+    private int hoursWorked;
+
+    public PartTimeEmployee(String name, int id, double hourlyRate, int hoursWorked) {
+        super(name, id);
+        this.hourlyRate = hourlyRate;
+        this.hoursWorked = hoursWorked;
+    }
+
+    public double getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public void setHourlyRate(double hourlyRate) {
+        this.hourlyRate = hourlyRate;
+    }
+
+    public int getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(int hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    @Override
+    public double calculateSalary() {
+        return hourlyRate * hoursWorked;
+    }
+
+    @Override
+    public void displayDetails() {
+        System.out.println(
+            "Part-Time Employee | Name: " + getName() +
+            " | ID: " + getId() +
+            " | Hourly: " + hourlyRate +
+            " | Hours: " + hoursWorked +
+            " | Salary: " + calculateSalary()
+        );
+    }
+}
